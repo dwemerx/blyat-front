0c0314165ba06fc1db19bc78c23a6d6f90700fca
dwemerx <70470685+dwemerx@users.noreply.github.com>
2020-11-03 10:45:31 +0300

    Add header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,26 @@ import Card from 'src/components/Card';
 
 const Home: React.FC = () => (
   <div className='h-screen overflow-y-scroll font-light text-center text-gray-700 bg-gradient-to-tr from-white via-gray-100 to-gray-100 grid-cols-none font-body'>
+    <header className='flex items-center justify-between px-8 py-4'>
+      <a className='text-3xl text-gray-800' href='https://blyat.org'>
+        Blyat
+      </a>
+      <div className='flex'>
+        <a className='mr-4 hover:text-gray-800' href='#services'>
+          Services
+        </a>
+        <a className='hover:text-gray-800' href='#phacyou'>
+          FAQ
+        </a>
+      </div>
+    </header>
     <div className='flex items-center justify-around h-screen mx-16 overflow-hidden bg-center bg-no-repeat bg-contain bg-timeline'>
       <div className='text-6xl text-gray-800'>Front</div>
     </div>
-    <div className='p-4 overflow-hidden justify-items-center grid'>
+    <div
+      id='services'
+      className='p-4 overflow-hidden justify-items-center grid'
+    >
       <Card
         title='FreshRSS'
         image={require('src/images/freshrss.png')}
