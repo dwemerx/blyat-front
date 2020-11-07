de670b7967028abe37cde513e7ef522f8a8aee61
dwemerx <70470685+dwemerx@users.noreply.github.com>
2020-11-07 06:46:11 +0300

    Fix the big circle look

--- src/components/Front.tsx
+++ src/components/Front.tsx
@@ -1,7 +1,7 @@
 import useMedia from 'use-media';
 
 const Front: React.FC = () => {
-  const isLarge = useMedia({ minWidth: 1024 });
+  const isLarge = useMedia({ minWidth: 1000 });
 
   return (
     <div>
@@ -13,7 +13,7 @@ const Front: React.FC = () => {
       <div className='absolute w-full bg-teal-700 h-x2 front-circle-2' />
       <div className='absolute w-full bg-teal-900 h-x2 front-circle-3' />
       <div className='h-screen mx-16 overflow-hidden grid lg:grid-cols-2 place-items-center'>
-        <div className='z-10 w-full text-3xl text-white lg:ml-16 lg:pl-6 lg:pr-32 xl:pb-64 xl:pr-64 max-w-screen-sm sm:text-5xl'>
+        <div className='z-10 w-full text-3xl text-white lg:ml-16 lg:pr-24 lg:pb-64 lg:mb-64 xl:pr-64 max-w-screen-sm sm:text-5xl'>
           All humans are born young. We need to change this{' '}
           <a
             className='text-transparent bg-clip-text bg-gradient-to-tl from-pink-500 via-red-500 to-yellow-500'
