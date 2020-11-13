74b410930ca8837e2d0efab506e1bf96d004acb0
dwemerx <70470685+dwemerx@users.noreply.github.com>
2020-11-13 10:52:38 +0300

    Update Services section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
+import { GitHub, Home } from 'react-feather';
 
 interface CardProps {
   title: string;
   description: string;
   image: string;
   serviceLink: string;
+  sourceLink: string;
+  mainInstanceLink?: string;
 }
 
 const Card: React.FC<CardProps> = ({
@@ -12,26 +15,52 @@ const Card: React.FC<CardProps> = ({
   description,
   image,
   serviceLink,
+  sourceLink,
+  mainInstanceLink,
 }) => (
-  <div className='z-10 flex items-center justify-between w-full px-4 py-2 my-4 bg-white max-w-screen-lg transition duration-500 ease-out hover:shadow-cardHover shadow-card rounded-3xl'>
-    <a
-      className='flex items-center justify-center w-24 h-24 bg-white focus:outline-none focus:shadow-outline rounded-xl'
-      href={serviceLink}
-      target='_blank'
-      rel='noreferrer'
-    >
-      <Image
-        width={90}
-        height={90}
-        src={image}
-        alt={`${title} Logo`}
-        title={title}
-      />
-    </a>
-    <div className='pr-4 text-right grid'>
+  <a
+    className='z-10 flex items-center justify-between w-full px-4 py-2 my-4 bg-white show-card-icons max-w-screen-lg transition duration-500 ease-out hover:shadow-cardHover shadow-card rounded-3xl'
+    href={serviceLink}
+    target='_blank'
+    rel='noreferrer'
+  >
+    <div className='flex items-center justify-start'>
+      <div className='flex items-center justify-center w-24 h-24 bg-white focus:outline-none focus:shadow-outline rounded-xl'>
+        <Image
+          width={90}
+          height={90}
+          src={image}
+          alt={`${title} Logo`}
+          title={title}
+        />
+      </div>
+      <div className='hidden ml-8 card-icons duration-500 md:flex '>
+        <a
+          className='w-16 h-16 mx-4 text-gray-500 rounded-full hover:text-gray-700 duration-300 grid place-items-center shadow-card hover:shadow-cardHover'
+          href={sourceLink}
+          target='_blank'
+          rel='noreferrer'
+          title={`${title} Source Code`}
+        >
+          <GitHub size={32} />
+        </a>
+        {mainInstanceLink && (
+          <a
+            className='w-16 h-16 mx-4 text-gray-500 rounded-full hover:text-gray-700 duration-300 grid place-items-center shadow-card hover:shadow-cardHover'
+            href={mainInstanceLink}
+            target='_blank'
+            rel='noreferrer'
+            title={`${title} Main Instance`}
+          >
+            <Home size={32} />
+          </a>
+        )}
+      </div>
+    </div>
+    <div className='w-40 pr-4 text-right sm:w-48 grid'>
       <a
         href={serviceLink}
-        className='w-full text-2xl focus:outline-none focus:shadow-outline '
+        className='w-full text-2xl focus:outline-none focus:shadow-outline'
         target='_blank'
         rel='noreferrer'
       >
@@ -39,7 +68,7 @@ const Card: React.FC<CardProps> = ({
       </a>
       <p className='text-xs text-gray-500'>{description}</p>
     </div>
-  </div>
+  </a>
 );
 
 export default Card;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,41 +11,52 @@ const Home: React.FC = () => (
       id='services'
       className='p-4 overflow-hidden justify-items-center grid'
     >
+      <div className='mb-8 text-6xl'>Services</div>
       <Card
         title='FreshRSS'
         image={'/images/freshrss.png'}
         serviceLink='https://freshrss.blyat.org'
-        description='A Powerfull RSS Reader'
+        description='Powerfull RSS Reader'
+        sourceLink='https://github.com/FreshRSS/FreshRSS'
       />
       <Card
         title='RSSHub'
         image={'/images/rsshub.png'}
         serviceLink='https://rsshub.blyat.org'
-        description='RSS Aggregator'
+        description='RSS Aggregator, written in Java'
+        sourceLink='https://github.com/DIYgod/RSSHub'
+        mainInstanceLink='https://rsshub.app/'
       />
       <Card
         title='RSSBridge'
         image={'/images/rssbridge.png'}
         serviceLink='https://rssbridge.blyat.org'
-        description='RSS Aggregator'
+        description='RSS Aggregator, written in PGP'
+        sourceLink='https://github.com/RSS-Bridge/rss-bridge'
       />
       <Card
         title='Reddit Top RSS'
         image={'/images/reddit.png'}
         serviceLink='https://rtr.blyat.org'
-        description='RSS Aggregator'
+        description='Reddit RSS Bridge'
+        sourceLink='https://github.com/johnwarne/reddit-top-rss'
+        mainInstanceLink='https://reddit-top-rss.herokuapp.com/?subreddit=pics'
       />
       <Card
         title='MORSS'
         image={'/images/rss.png'}
         serviceLink='https://morss.blyat.org'
-        description='RSS Aggregator'
+        description='Get Full Articles from Your Feeds'
+        sourceLink='https://github.com/pictuga/morss'
+        mainInstanceLink='https://morss.it/'
       />
       <Card
         title='HN RSS'
         image={'/images/hnrss.png'}
         serviceLink='https://hnrss.org'
-        description='RSS Aggregator'
+        description='Advanced HN RSS Bridge'
+        sourceLink='https://github.com/hnrss/hnrss'
+        mainInstanceLink='https://hnrss.org'
       />
     </div>
   </Layout>
