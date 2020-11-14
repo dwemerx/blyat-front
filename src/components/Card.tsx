f962992b01d52c2d01d3d57e5935feba84819d29
dwemerx <70470685+dwemerx@users.noreply.github.com>
2020-11-14 16:26:40 +0300

    Improve card look

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -19,7 +19,7 @@ const Card: React.FC<CardProps> = ({
   mainInstanceLink,
 }) => (
   <a
-    className='z-10 flex items-center justify-between w-full px-4 py-2 my-4 bg-white border border-gray-300 hover:border-gray-500 show-card-icons max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg transition duration-500 ease-out rounded-3xl'
+    className='z-10 flex items-center justify-between w-full px-4 py-0 my-4 bg-white border border-gray-300 hover:border-gray-500 show-card-icons max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg transition duration-500 ease-out rounded-3xl'
     href={serviceLink}
     target='_blank'
     rel='noreferrer'
@@ -27,8 +27,8 @@ const Card: React.FC<CardProps> = ({
     <div className='z-10 flex items-center justify-start'>
       <div className='z-10 flex items-center justify-center w-24 h-24 bg-white focus:outline-none focus:shadow-outline rounded-xl'>
         <Image
-          width={90}
-          height={90}
+          width={70}
+          height={70}
           src={image}
           alt={`${title} Logo`}
           title={title}
@@ -36,7 +36,7 @@ const Card: React.FC<CardProps> = ({
       </div>
       <div className='z-10 hidden ml-8 card-icons duration-500 md:flex '>
         <a
-          className='z-10 w-12 h-12 mx-4 text-gray-400 border border-gray-300 rounded-full hover:border-gray-500 hover:text-gray-700 duration-300 grid place-items-center'
+          className='z-10 w-12 h-12 mx-4 text-gray-400 border border-gray-300 rounded-full hover:border-gray-500 hover:text-gray-500 duration-300 grid place-items-center'
           target='_blank'
           rel='noreferrer'
           title={`${title} Source Code`}
@@ -46,7 +46,7 @@ const Card: React.FC<CardProps> = ({
         </a>
         {mainInstanceLink && (
           <a
-            className='z-10 w-12 h-12 mx-4 text-gray-400 border border-gray-300 rounded-full hover:border-gray-500 hover:text-gray-700 duration-300 grid place-items-center'
+            className='z-10 w-12 h-12 mx-4 text-gray-400 border border-gray-300 rounded-full hover:border-gray-500 hover:text-gray-500 duration-300 grid place-items-center'
             target='_blank'
             rel='noreferrer'
             title={`${title} Main Instance`}
