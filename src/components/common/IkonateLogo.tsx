f2f48af914b90773f39923110c9be258b4a507cf
dwemerx <70470685+dwemerx@users.noreply.github.com>
2020-12-08 14:54:43 +0300

    Update Footer

---
+++ src/components/common/IkonateLogo.tsx
@@ -0,0 +1,21 @@
+function IkonateLogo() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width={24}
+      height={24}
+      viewBox='2 2 20 20'
+      aria-labelledby='Ikonate Logo'
+      stroke='#444'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      fill='none'
+      color='fill-current'
+    >
+      <title>{'Ikonate Logo'}</title>
+      <path d='M21 14h-3l-3-7-5 10-3-6-2 3H3' />
+    </svg>
+  );
+}
+
+export default IkonateLogo;
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,9 +1,22 @@
-import { GitHub } from 'react-feather';
+import { Feather, GitHub } from 'react-feather';
+import IkonateLogo from 'src/components/common/IkonateLogo';
 
 const Footer: React.FC = () => (
   <footer className='relative flex justify-center p-8 text-white text-gray-700 fill-current hover:text-gray-800'>
     <div className='text-gray-500'>
       BLYAT - a service provider you probably shoudn&apos;t trust
+    </div>
+    <div className='absolute flex-col hidden w-16 h-full pb-16 lg:flex sm:left-0 sm:pl-8 sm:pt-0 focus:outline-none focus:shadow-outline'>
+      <div className='flex mb-2 align-middle'>Icons:</div>
+      <div className='flex'>
+        <a href='https://github.com/feathericons/feather' className='pr-2'>
+          <Feather strokeWidth={1.5} size={24} />
+        </a>
+        <p> & </p>
+        <a href='https://github.com/mikolajdobrucki/ikonate' className='pl-2'>
+          <IkonateLogo />
+        </a>
+      </div>
     </div>
     <a
       href='https://github.com/dwemerx/blyat-front'
