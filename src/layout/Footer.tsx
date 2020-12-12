cdffa6f071b4f8c9154ed5625da4e3416d1e64b7
dwemerx <70470685+dwemerx@users.noreply.github.com>
2020-12-12 08:57:04 +0300

    Update Footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,11 +2,19 @@ import { Feather, GitHub } from 'react-feather';
 import IkonateLogo from 'src/components/common/IkonateLogo';
 
 const Footer: React.FC = () => (
-  <footer className='relative flex justify-center p-8 text-white text-gray-700 fill-current hover:text-gray-800'>
+  <footer className='relative flex flex-col justify-center p-8 text-white text-gray-700 align-middle fill-current hover:text-gray-800'>
     <div className='text-gray-500'>
       BLYAT - a service provider you probably shoudn&apos;t trust
     </div>
-    <div className='absolute flex-col hidden w-16 h-full pb-16 lg:flex sm:left-0 sm:pl-8 sm:pt-0 focus:outline-none focus:shadow-outline'>
+    <div className='mt-6 text-gray-500'>
+      <a
+        className='text-gray-500 hover:text-gray-700'
+        href='https://uptime.blyat.org'
+      >
+        Uptime
+      </a>
+    </div>
+    <div className='absolute flex-col hidden w-16 h-full top-6 lg:flex sm:left-0 sm:pl-8 sm:pt-0 focus:outline-none focus:shadow-outline'>
       <div className='flex mb-2 align-middle'>Icons:</div>
       <div className='flex'>
         <a href='https://github.com/feathericons/feather' className='pr-2'>
@@ -22,7 +30,7 @@ const Footer: React.FC = () => (
       href='https://github.com/dwemerx/blyat'
       target='_blank'
       rel='noreferrer'
-      className='absolute h-full pt-16 sm:right-0 sm:pr-8 sm:pt-0 focus:outline-none focus:shadow-outline'
+      className='flex justify-center h-full mt-6 sm:mt-0 sm:absolute sm:right-0 sm:top-12 sm:pr-8 sm:pt-0 focus:outline-none focus:shadow-outline'
     >
       <GitHub strokeWidth={1.5} size={24} />
     </a>
