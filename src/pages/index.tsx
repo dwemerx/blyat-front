025314bb68e9b1715c26633dfddae4664693159d
dwemerx <70470685+dwemerx@users.noreply.github.com>
2022-03-06 06:38:49 +0100

    End

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
-import FAQ from 'src/components/FAQ';
-import Front from 'src/components/Front';
-import Services from 'src/components/Services';
-import Testimonials from 'src/components/Testimonials';
-import Layout from 'src/layout/Layout';
+// import FAQ from 'src/components/FAQ';
+// import Front from 'src/components/Front';
+// import Services from 'src/components/Services';
+// import Testimonials from 'src/components/Testimonials';
+// import Layout from 'src/layout/Layout';
 
 const Home: React.FC = () => (
-  <Layout>
-    <Front />
-    <Testimonials />
-    <Services />
-    <FAQ />
-  </Layout>
+  // <Layout>
+  //   <Front />
+  //   <Testimonials />
+  //   <Services />
+  //   <FAQ />
+  // </Layout>
+  <div>
+    Because Putin is into commiting war crimes, it is not possible to pay for
+    the infrastructure anymore. Because of this, the project can no longer work.
+    Thank you for using BLYAT.
+  </div>
 );
 
 export default Home;
